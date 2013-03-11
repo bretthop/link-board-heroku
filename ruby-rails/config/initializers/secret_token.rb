8a27d3bafe1f68fe7b491bf96ab5c31fa2680c53
bretthop <brett.hoppz@gmail.com>
2013-03-12 08:34:21 +1100

    * Added basic Rails app * Copied static webapp files over

---
+++ ruby-rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyRails::Application.config.secret_token = '8efe292cf89bfbe948a89cbc86eda82013379be78da1b589019af8ec29d249bc2f089f6e1396c023f16341d6407b09fedc0cb865aca55a36c4dd354ba18da2c3'
