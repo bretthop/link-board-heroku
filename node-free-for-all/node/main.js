aab7d462b65d55f70f48f527be7b6e8ed44ba200
bretthop <brett.hoppz@gmail.com>
2013-03-27 08:33:15 +1100

    * Started porting connect-specific code to vanilla node code

--- node-free-for-all/node/main.js
+++ node-free-for-all/node/main.js
@@ -1,4 +1,3 @@
-// TODO: Make file paths relative
 var webapp_root = __dirname + '\\..\\webapp';
 
 var connect = require('connect'),
@@ -8,6 +7,38 @@ var connect = require('connect'),
     linksEndpoint = require('./endpoints/links-endpoint.js'),
     urlUtils = require('./utils/url-utils');
 
+
+var http = require('http'),
+    send = require('send'),
+    url = require('url');
+
+http.createServer(function(req, res) {
+    if (req.url.length >= '/api'.length && req.url.substr(0, '/api'.length) == '/api') {
+        urlUtils.parseParams()(req, res, function() {
+            securityFilter.process('/api/users', 'POST')(req, res, function() {
+                switch (req.url) {
+                    case '/api/users':
+                        usersEndpoint.process(req, res);
+                        break;
+                    case '/api/links':
+                        linksEndpoint.process(req, res);
+                        break;
+                    case '/api/linkGroups':
+                        linkGroupsEndpoint.process(req, res);
+                        break;
+                }
+            });
+        });
+    }
+    else { // Serve static files
+        send(req, url.parse(req.url).pathname)
+            .root(webapp_root)
+            .pipe(res);
+    }
+}).listen(3000);
+
+// TODO: This is being replaced by a vanilla node app (see the node-express backend for an implementation using a framework)
+/*
 connect()
     .use(urlUtils.parseParams())
     .use(connect.json())// Parses incoming JSON requests and adds it to the request object as 'req.body'
@@ -16,4 +47,5 @@ connect()
     .use('/api/linkGroups', linkGroupsEndpoint.process)
     .use('/api/links', linksEndpoint.process)
     .use(connect.static(webapp_root))
-    .listen(4444);
+    .listen(3000);
+*/
