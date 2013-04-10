4471a7d2e4f0beb0863d1c74f631381cefbc700a
bretthop <brett.hoppz@gmail.com>
2013-04-11 08:53:14 +1000

    * Added API handlers (they are just stubs atm) * Added basic example of creating an object and serialising it to JSON

--- go-vanilla/go/main.go
+++ go-vanilla/go/main.go
@@ -1,11 +1,56 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"encoding/json"
 )
+
+// TODO: Move to model package
+type User struct {
+    Id          int
+    Username    string
+    Password    string
+    Email       string
+    FirstName   string
+    LastName    string
+}
+
+func usersHandler(w http.ResponseWriter, r *http.Request) {
+    u := User {
+        Id: 1,
+        Username: "temp",
+        Password: "temp",
+        Email: "temp",
+        FirstName: "temp",
+        LastName: "temp",
+    }
+
+    res, err := json.Marshal(u)
+
+    if err != nil {
+        fmt.Println("ERROR: ", err)
+    }
+
+	fmt.Fprintf(w, string(res))
+}
+
+func linksHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Links - TODO!")
+}
+
+func linkGroupsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Link Groups - TODO!")
+}
 
 func main() {
 	webapp_root := "..\\webapp"
 	
-	http.ListenAndServe(":8080", http.FileServer(http.Dir(webapp_root)))
+	http.HandleFunc("/api/users", usersHandler)
+	http.HandleFunc("/api/links", linksHandler)
+	http.HandleFunc("/api/linkGroups", linkGroupsHandler)
+	
+	http.Handle("/", http.FileServer(http.Dir(webapp_root)))
+	
+	http.ListenAndServe(":8080", nil)
 }
